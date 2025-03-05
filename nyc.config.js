0552141fe86c92fc9107e22924ca60743570bd41
Yahav Levy <yahav.levy@bringg.com>
2025-03-05 11:18:19 +0200

    changed cov to 50% because a lot of our code is made of ai tools

--- nyc.config.js
+++ nyc.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	include: ['app/**'],
 	exclude: ['app/**/*.test.ts', 'app/**/test/**'],
 	'check-coverage': true,
+	lines: 50,
 	all: true,
 	reporter: ['lcov', 'text', 'text-summary'],
 	'report-dir': 'coverage'
