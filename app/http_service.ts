24bcee967f0a428bac228e20821a7e3891d84528
Yahav Levy <yahav.levy@bringg.com>
2025-04-03 15:47:18 +0300

    lint fixes

--- app/http_service.ts
+++ app/http_service.ts
@@ -1,8 +1,8 @@
 import HttpServiceBuilder from '@bringg/service/lib/http_service_builder';
 import cors from 'cors';
 
-import { PROJECT_ROOT_PATH } from './project-root-path';
 import { IS_DEV } from './common/constants';
+import { PROJECT_ROOT_PATH } from './project-root-path';
 
 export const httpService = new HttpServiceBuilder()
 	.useContext()
